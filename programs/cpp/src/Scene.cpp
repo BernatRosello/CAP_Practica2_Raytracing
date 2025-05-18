da28d4430ecdda1ec3737d1ed40bd34775cc121d
BernatRosello <63212761+BernatRosello@users.noreply.github.com>
2025-05-18 13:54:18 +0200

    update cpp scene getcolor too

--- programs/cpp/src/Scene.cpp
+++ programs/cpp/src/Scene.cpp
@@ -29,6 +29,6 @@ Vec3 Scene::getSceneColor(const Ray& r, int depth) {
 	else {
 		Vec3 unit_direction = unit_vector(r.direction());
 		float t = 0.5f * (unit_direction.y() + 1.0f);
-		return (1.0f - t) * Vec3(1.0f, 1.0f, 1.0f) + t * Vec3(0.5f, 0.7f, 1.0f);
+		return (1.0f - t) * inf + t * sky;
 	}
 }
